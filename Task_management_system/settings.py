9e30a6d00ca8ceb90dffaad37c5d28ce979b584c
Wilfred1213 <99550155+Wilfred1213@users.noreply.github.com>
2023-07-13 21:48:04 +0100

    Update settings.py
    password change

--- Task_management_system/settings.py
+++ Task_management_system/settings.py
@@ -151,7 +151,7 @@ EMAIL_PORT =465
 EMAIL_USE_SSL=True
 EMAIL_USE_TLS = False
 EMAIL_HOST_USER = 'mathiaswilfred7@gmail.com'
-EMAIL_HOST_PASSWORD= 'kpvwrumwpoucrdft'
+EMAIL_HOST_PASSWORD= '****'
 
 
 
